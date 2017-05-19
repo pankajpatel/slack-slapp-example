1b19cccc57b863e8b0d9793ea957cc58e1acdc78
christian-bick <christian.bick@actano.de>
2017-05-19 19:29:17 +0200

    Fix help text

--- bot/help-actions.js
+++ bot/help-actions.js
@@ -3,7 +3,7 @@ import catchAsync from '../lib/async'
 const useHelpActions = (slapp) => {
   slapp.message(/^(hi|hello|hey|help).*/, ['direct_mention', 'direct_message'], catchAsync((message) => {
     message.say({
-      text: "Hi! Use */events* _event name_ to create a new event",
+      text: "Hi! Use */event* _event name_ to create a new event",
     })
   }))
 }
