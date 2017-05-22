5362a6dcdedaec05d5128f9ea97956bb3b92e181
christian-bick <christian.bick@actano.de>
2017-05-22 10:37:47 +0200

    Add "join" button with a simple action

--- bot/event-actions.js
+++ bot/event-actions.js
@@ -3,8 +3,23 @@ import catchAsync from '../lib/async'
 const useEventActions = (slapp) => {
   slapp.command('/event', catchAsync((message, value) => {
     message.say({
-      text: `There is a new event:  *${value}*\n`,
+      text: `<@${message.meta.user_id}> created a new event:\n *${value}*`,
+      attachments: [{
+        attachment_type: 'default',
+        callback_id: 'event',
+        text: `Do you want to join this event?`,
+        actions: [{
+          type: 'button',
+          name: 'join',
+          text: 'Join',
+          value
+        }]
+      }]
     })
+  }))
+
+  slapp.action('event', 'join', catchAsync((message, value) => {
+    message.say(`<@${message.meta.user_id}> joins ${value}`)
   }))
 }
 
