afd92293afbe734898eea38d6f2bc1ab505cf8e4
christian-bick <christian.bick@actano.de>
2017-05-22 16:49:20 +0200

    Move on step back

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ import 'babel-register'
 import express from 'express'
 import Slapp from 'slapp'
 import BeepBoopContext from 'slapp-context-beepboop'
-import loki from 'lokijs'
 
 import useHelpActions from './bot/help-actions'
-import useEventActions from './bot/event-actions'
 
 const app = express()
 
@@ -21,19 +19,4 @@ app.listen(process.env.PORT)
 
 console.log('Slack App is running on port ', process.env.PORT)
 
-const db = new loki('event.db', {
-  persistenceAdapter: 'fs',
-  autoload: true,
-  autoloadCallback: onDatabaseLoaded,
-  autosave: true,
-  autosaveInterval: 100, // 10 seconds
-})
-
-function onDatabaseLoaded() {
-  let events = db.getCollection('events')
-  if (!events) {
-    events = db.addCollection('events')
-  }
-  useHelpActions(slapp)
-  useEventActions(slapp, events)
-}
+useHelpActions(slapp)
